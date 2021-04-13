db019dfdd232a5dc6cd33e09909b60885e93ee89
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-04-13 20:43:25 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -10,35 +10,27 @@ public class Conta {
 		return agencia;
 	}
 
-
 	public void setAgencia(String agencia) {
 		this.agencia = agencia;
 	}
-
 
 	public int getNumero() {
 		return numero;
 	}
 
-
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
 
-
-	
 	void deposita(double valor){
 		this.setSaldo(this.getSaldo() + valor);
 	}
-
 
 	public double getSaldo() {
 		return saldo;
 	}
 
-
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
 }
