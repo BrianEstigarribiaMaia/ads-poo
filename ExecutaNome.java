194c57529b1c4f38dea5b8e5497486b6128beff5
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-04-13 20:47:50 -0300

    Update ExecutaNome.java

--- ExecutaNome.java
+++ ExecutaNome.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-//ordenação de arraylist simples
-
 public class ExecutaNome {
 
 	public static void main(String[] args) {
 		List<String> lista = new ArrayList<>();
+		
 		lista.add("Luke");
 		lista.add("Shiwie");
 		lista.add("Leia");
