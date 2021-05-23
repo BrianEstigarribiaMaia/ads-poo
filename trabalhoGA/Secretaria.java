872192a99c3e546e6a52f0aff8ddaf4d488814df
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-23 19:09:12 -0300

    Add files via upload

---
+++ trabalhoGA/Secretaria.java
@@ -0,0 +1,10 @@
+package br.com.ftec.poo.trabalhoga;
+
+public class Secretaria extends Funcionario{
+
+	public Secretaria(String nome, String cracha, double salario, double dissidioAnunal) {
+		super(nome, cracha, salario, dissidioAnunal);
+		
+	}
+
+}
