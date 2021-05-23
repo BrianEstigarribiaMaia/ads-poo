f729a966f4233d3908aa8411a3b443e5e4c96562
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-23 19:08:18 -0300

    Add files via upload

---
+++ Desenvolvedor.java
@@ -0,0 +1,10 @@
+package br.com.ftec.poo.trabalhoga;
+
+public class Desenvolvedor extends Funcionario{
+
+	public Desenvolvedor(String nome, String cracha, double salario, double dissidioAnunal) {
+		super(nome, cracha, salario, dissidioAnunal);
+		
+	}
+
+}
