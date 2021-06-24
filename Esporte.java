461532e742bd7c5020116abc04e718055897f865
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-06-24 16:34:43 -0300

    Add files via upload

---
+++ Esporte.java
@@ -0,0 +1,9 @@
+package br.com.ftec.poo.exercicios.enums;
+/*Criar um enum para 3 modalidades de esporte.
+*- No main exiba a sua modalidade favorita.
+*/
+
+public enum Esporte {
+
+	futebol, taekwondo, basquete;
+}
